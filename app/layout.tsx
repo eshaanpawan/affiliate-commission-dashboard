8ceee1dd9428715463297a8be6e9e0f00dc9be36
NiladriHazra <iladrivit@gmail.com>
2026-08-07 22:39:44 +0530

    Rebrand tab to Runable Affiliates with Runable mark favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Affiliate Commission Dashboard",
+  title: "Runable Affiliates",
   description: "Runable affiliate performance dashboard",
 };
 
